ff654c4ef15bf38154864343c1470b28f96247db
Diwakar-Dhawal <108503741+Diwakar-Dhawal@users.noreply.github.com>
2023-09-20 15:12:37 +0530

    Commit

--- Q287.java
+++ Q287.java
@@ -1,3 +1,8 @@
+/*  Q287. Find the Duplicate Number
+    Difficulty : Medium
+    Problem Statement : Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.There is only one repeated number in nums, return this repeated number.
+                        You must solve the problem without modifying the array nums and uses only constant extra space.*/
+
 import java.util.HashSet;
 
 public class Q287 {
